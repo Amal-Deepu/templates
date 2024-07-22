cee62915c50fe1f0b390ad55920a09437f6af266
amal <amaldeepu1@gmail.com>
2024-07-22 14:18:29 +0530

    frontendupdated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React from "react";
 import Logo from "../Assets/Logo.svg";
-import { HiOutlineBars3 } from "react-icons/hi2";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Divider from "@mui/material/Divider";
+
 
 const Navbar = () => {
-  const [openMenu, setOpenMenu] = useState(false);
+  
  
   return (
     <nav>
@@ -15,20 +12,8 @@ const Navbar = () => {
         <img src={Logo} alt="" />
       </div>
       
-      <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-      </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
-        >
-          
-          <Divider />
-        </Box>
-      </Drawer>
+      
+    
     </nav>
   );
 };
